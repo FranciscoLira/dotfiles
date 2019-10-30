7097c82fff8bd31339b8697ffb61ccaa056cc8eb
Francisco Lira <arjot12@gmail.com>
2019-10-30 19:19:45 +0000

    Update asdf for lira

--- asdf/install.sh
+++ asdf/install.sh
@@ -26,13 +26,10 @@ echo_info "Update asdf itself..."
 asdf update
 
 echo_info "Add asdf plugins..."
-asdf plugin-add R
 asdf plugin-add golang
 asdf plugin-add haskell
-asdf plugin-add julia
 asdf plugin-add nodejs
 asdf plugin-add ruby
-asdf plugin-add rust
 
 echo_info "Update all plugins..."
 asdf plugin-update --all
